0cc470f14e497037e249bfbb7e6c4eb6d4ce9a4f
Stefan Mettler <stefan.mettler92@gmail.com>
2024-02-29 21:05:37 +0100

    Added more logging to pipeline and created functions for handling accuracy, integrity and some helper functions to support this purpose

--- models/models.py
+++ models/models.py
@@ -2,7 +2,7 @@ import os
 import logging
 import pandas as pd
 import json
-from utils.utils import remove_columns_with_missing
+from utils.utils import merge_to_date_time_col, remove_columns_with_missing, remove_rows_with_duplicate_col_conditionally
 
 
 class Pipeline:
@@ -36,13 +36,15 @@ class Pipeline:
 
     def load_data(self, file):
         """
-        Load CSV from the specified path and store it in the dataframe.
+        Load CSV, or other data file, from the specified path and store it in the dataframe.
         """
         logging.info(f"Loading data from {file}")
         self.df = pd.read_csv(f"{self.config['inputdir']}{file}", low_memory=False)
 
     def handling_accuracy(self):
         # handle data type and formats
+        self.df['issue_month'] = self.df['issue_month'].replace('Octxyz', 'Oct')
+        merge_to_date_time_col(df=self.df, year_col='issue_year', month_col='issue_month', datetime_col='issue_date')
         pass
 
     def handling_completeness(self):
@@ -54,7 +56,25 @@ class Pipeline:
 
     def handling_integrity(self):
         # handle duplicates
+        # drop first column, which contains a row index
+        # (prevents detection of duplicate rows)
+        self.df = self.df.drop(columns='Unnamed: 0')
+        rows_before = self.df.shape[0]
+        #drop rows where all values are duplicates
+        self.df = self.df.drop_duplicates()
+        logging.info(f"Removed {rows_before - self.df.shape[0]} rows with duplicates")
+        condition = self.df['installment'] > 10000
+        rows_before = self.df.shape[0]
+        self.df = remove_rows_with_duplicate_col_conditionally(df=self.df, col='member_id', condition=condition)
+        logging.info(f"Removed {rows_before - self.df.shape[0]} rows which had the same " + 
+                     "member_id as another entry and the monthly installment was bigger than 10'000")
+
+
         # handle inconsistent data
+        regex = r'^.{0,1}$|^.{3,}$'
+        self.df['sub_grade'] = self.df['sub_grade'].replace(regex, None, regex=True)
+        #self.df['sub_grade'] = self.df['sub_grade'].replace('ALPHA_CENTAURI', None)
+        logging.info("Removed nonsensical values from the subgrade column")
         pass
 
     def handling_text(self):
@@ -77,8 +97,8 @@ class Pipeline:
         for file in self.input_file_list:
             self.load_data(file)
             # self.handling_text()
-            # self.handling_accuracy()
-            # self.handling_integrity()
+            self.handling_accuracy()
+            self.handling_integrity()
             self.handling_completeness()
             # self.data_protection()
             self.save_data(file)
--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+
 def find_missing_cols(df, threshold=1):
     """ Find Columns with all (or threshold) missing
         Parameters:
@@ -23,3 +26,92 @@ def remove_columns_with_missing(df, threshold=1):
     """
     cols_to_remove = find_missing_cols(df, threshold)
     return df.drop(columns=cols_to_remove), cols_to_remove
+
+def remove_rows_with_duplicate_col_conditionally(df, col, condition):
+    """ Remove Rows where the column col id duplicate and the condition applies
+        Parameters:
+        -----------
+        df : pandas.DataFrame
+        The input DataFrame to analyze for duplicate values.
+        col: string 
+        the col to check for duplicate values
+        condition: _bool
+        the condition the duplicate rows have to fulfill to be removed
+        Returns:
+        --------
+        pandas.DataFrame
+    """
+    # filter for all entries containing duplicated col order by col
+    duplicated_cols = df[df.duplicated(subset=col, keep=False)][col].unique().tolist()
+    return df[~df[col].isin(duplicated_cols) | df[col].isin(duplicated_cols) & ~condition]
+    
+#currently does not work as intendend, still working on it
+def replace_non_dependend_cols(df, col, subcol, predicate):
+    """ Replaces entries where the values of two cols that should be dependend on one another, are not dependend
+        Parameters:
+        -----------
+        df : pandas.DataFrame
+        The input DataFrame to analyze for duplicate values.
+        col: string 
+        the first of the two cols to compare
+        subcol: string 
+        the second of the two cols to compare
+        predicat: function
+        the predicate to compare the two cols with 
+        Returns:
+        --------
+        pandas.DataFrame
+    """
+    df_without_independend_entries = df.copy()
+    _pred = df_without_independend_entries[subcol].str.match(df_without_independend_entries[col]+'\d{1}')
+    df_without_independend_entries[subcol] = np.where(_pred, None,  df_without_independend_entries[subcol])
+    return df_without_independend_entries
+
+def has_string_type(s: pd.Series) -> bool:
+    """ Returns true if column is of type string
+        Parameters:
+        -----------
+        s: pd.Series
+        The series to be investigated
+    """
+    if pd.StringDtype.is_dtype(s.dtype):
+        # StringDtype extension type
+        return True
+
+    if s.dtype != "object":
+        # No object column - definitely no string
+        return False
+
+    try:
+        s.str
+    except AttributeError:
+        return False
+
+    # The str accessor exists, this must be a String column
+    return True
+
+def merge_to_date_time_col(df, year_col, month_col, datetime_col='merged_datetime'):
+    """ Takes two colums, one containing a year and one containing a month and 
+        create a new column combining the values from the year and month column to a datetime and dropping the old columns
+        Parameters:
+        -----------
+        df: pandas.DataFrame
+        the frame on which we operate
+        year_col: string
+        name of the col that contains the year value
+        month_col: string
+        name of the col that contains the month value
+        Returns:
+        --------
+        pandas.DataFrame
+    """
+    # sanity check, that we are working with string cols
+    # TODO: make day column optional
+    if(not has_string_type(df[year_col]) or not has_string_type(month_col)):
+        return
+    # create new variable datetime_col with data type datetime
+    df_with_new_col = df.copy()
+    df_with_new_col[datetime_col] = pd.to_datetime(df[year_col].astype(str) + '-' + df[month_col], format='%Y-%b')
+    # drop no longer needed cols
+    return df_with_new_col.drop(columns=[year_col, month_col])
+
